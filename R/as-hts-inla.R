3b539d9aafb94b909dd8045035126bb87ab80462
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-06-29 17:27:03 +0800

    add docs for formula in as_hts_inla

--- R/as-hts-inla.R
+++ R/as-hts-inla.R
@@ -1,5 +1,8 @@
 #' @title Create object of class "hts_inla"
+#'
 #' @param x model from inlabru
+#' @param formula formula passed to `fit_hts()`
+#'
 #' @return hts_inla model
 #' @author Nicholas Tierney
 #' @note internal
