756be563e6c2d6ddeca387ad3b011aaacff7bd08
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-06-29 15:49:51 +0800

    skip test_if_valid_family on CI

--- tests/testthat/test-test-if-valid-family.R
+++ tests/testthat/test-test-if-valid-family.R
@@ -1,5 +1,9 @@
 test_that("test_if_valid_family works", {
   expect_silent(test_if_valid_family("gaussian"))
+})
+
+test_that("test_if_valid_family with expect_snapshot()", {
+  skip_on_ci()
   expect_snapshot(
     error = TRUE,
     test_if_valid_family("gauss")
