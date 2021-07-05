6c30f107a38765085cbfce2a00cfb5230c1f7851
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-07-05 11:33:44 +0800

    update fitted.hts_inla to return only the mean value

--- R/fitted.R
+++ R/fitted.R
@@ -1,18 +1,21 @@
-#' Extract fitted values from model object
+#' Extract fitted values from `hts_inla` model object
+#'
+#' This returns the fitted
 #'
 #' @param object hts_inla object
-#' @param fit default is "mean". Options are: "mean", "sd", "0.025quant", "0.5quant", "0.975quant", and "mode".
 #' @param exponentiate default FALSE
-#' @param ... extra arguments. Currently not used
+#' @param ... extra arguments. Currently not used.
 #'
 #' @export
+#' @examples
+#' head(fitted(hts_example_model))
 fitted.hts_inla <- function(object,
-                            fit = "mean",
                             exponentiate = FALSE,
                             ...){
 
-  data_nrow <- nrow(object$data)
-  model_fit <- object$summary.fitted.values[[fit]][seq_len(data_nrow)]
+  names <- rownames(object$summary.fitted.values)
+  keep <- grepl("^fitted.APredictor", names)
+  model_fit <- object$summary.fitted.values[keep, "mean"]
 
   if (exponentiate){
     return(exp(model_fit))
